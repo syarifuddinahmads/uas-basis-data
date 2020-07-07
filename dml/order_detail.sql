d3f65a429908af384e16b621ba798c72c8bbccdf
Achmad Muchlasin <47469965+muchlaseen@users.noreply.github.com>
2020-07-08 00:17:25 +0700

    order & order_detail fixed

--- dml/order_detail.sql
+++ dml/order_detail.sql
@@ -1,8 +1,8 @@
 insert all
-    into order_detail(id_order_detail,id_order,id_coffe_order,quantity_order_detail,amount_order_detail,created_at,modified_at) values
-    (1,1,1,1,20000,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
-    into order_detail(id_order_detail,id_order,id_coffe_order,quantity_order_detail,amount_order_detail,created_at,modified_at) values
-    (2,2,1,1,20000,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
-    into order_detail(id_order_detail,id_order,id_coffe_order,quantity_order_detail,amount_order_detail,created_at,modified_at) values
-    (3,3,1,1,20000,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
+    into order_detail(id_order_detail,id_order,id_coffe,quantity_order_detail,amount_order_detail,created_at,modified_at) values
+    (1,1,1,1,22000,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
+    into order_detail(id_order_detail,id_order,id_coffe,quantity_order_detail,amount_order_detail,created_at,modified_at) values
+    (2,2,3,1,15000,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
+    into order_detail(id_order_detail,id_order,id_coffe,quantity_order_detail,amount_order_detail,created_at,modified_at) values
+    (3,3,5,1,20000,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
 select * from dual;
