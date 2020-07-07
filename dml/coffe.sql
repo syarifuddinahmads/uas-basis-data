54509cbfd4d0b5ddf6f3b92fc41a90012e4da5c7
Achmad Muchlasin <47469965+muchlaseen@users.noreply.github.com>
2020-07-07 23:40:10 +0700

    coffe & coffe_type fixed

--- dml/coffe.sql
+++ dml/coffe.sql
@@ -1,7 +1,7 @@
 insert all
-    into coffe(id_coffe,name_coffe,amount_coffe,type_coffe,created_at,modified_at) values(1,'Milk Coffe',20000,1,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
-    into coffe(id_coffe,name_coffe,amount_coffe,type_coffe,created_at,modified_at) values(2,'Milk Coffe',25000,2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
-    into coffe(id_coffe,name_coffe,amount_coffe,type_coffe,created_at,modified_at) values(3,'Milk Coffe',15000,3,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
-    into coffe(id_coffe,name_coffe,amount_coffe,type_coffe,created_at,modified_at) values(4,'Milk Coffe',15000,1,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
-    into coffe(id_coffe,name_coffe,amount_coffe,type_coffe,created_at,modified_at) values(5,'Milk Coffe',10000,2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
+    into coffe(id_coffe,name_coffe,amount_coffe,type_coffe,created_at,modified_at) values(1,'Ristretto Bianco',22000,1,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
+    into coffe(id_coffe,name_coffe,amount_coffe,type_coffe,created_at,modified_at) values(2,'Caramel Macchiato',25000,2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
+    into coffe(id_coffe,name_coffe,amount_coffe,type_coffe,created_at,modified_at) values(3,'Caffe Latte',15000,3,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
+    into coffe(id_coffe,name_coffe,amount_coffe,type_coffe,created_at,modified_at) values(4,'Espresso Con Panna',18000,1,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
+    into coffe(id_coffe,name_coffe,amount_coffe,type_coffe,created_at,modified_at) values(5,'Cocoa Cloud Macchiato',20000,2,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
 select * from dual;
